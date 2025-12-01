c8d5bc1ae74f97ca13697a63c0c722bd0ea2ca57
Lapma <lapostyanm@kkszki.hu>
2025-12-01 09:42:36 +0100

    AddnewUser service is in progress.

---
+++ Services/IServices/UserService.cs
@@ -0,0 +1,48 @@
+﻿using MySqlX.XDevAPI.Common;
+using UserRoleApi.Models;
+using UserRoleApi.Models.Dtos;
+
+namespace UserRoleApi.Services.IServices
+{
+    public class UserService : IUser
+    {
+        private readonly UserDBContext _context;
+        public UserService(UserDBContext context)
+        {
+            _context = context;
+        }
+        public async Task<object> AddNewUser(AddUserDto addUserDto)
+        {
+			try
+			{
+                var result = new ResultResponseDto();
+                var user = new User
+				{
+                    Id = Guid.NewGuid(),
+                    Name = addUserDto.Name,
+                    Email = addUserDto.Email,
+                    Password = addUserDto.Password
+                };
+                
+                if (user != null)
+                {
+                    await _context.Users.AddAsync(user);
+                    await _context.SaveChangesAsync();
+                    result.message = "User added successfully";
+                    result.result = user;
+                    return result;
+                }
+                result.message = "User add is failed";
+                result.result = user;
+                return result;
+
+            }
+			catch (Exception ex)
+            {
+                var result = new ResultResponseDto();
+                result.message = ex.Message;
+                return result;
+            }
+        }
+    }
+}
